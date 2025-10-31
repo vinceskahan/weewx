1c3bf961ebdccf22b6e9425600dcab2ef37ba417
Vince Skahan <vinceskahan@gmail.com>
2025-10-31 10:18:59 -0700

    download then install

--- weewx-vagrant/deb13/provision-belchertown.sh
+++ weewx-vagrant/deb13/provision-belchertown.sh
@@ -1,17 +1,21 @@
 #!/bin/bash 
 
-# this is belchertown from my stashed tgz of upstream
-URL="https://github.com/vinceskahan/upstream-repos/blob/main/weewx-belchertown.tgz"
+# this is belchertown from my stashed zip of upstream
+URL="https://github.com/vinceskahan/upstream-repos/raw/refs/heads/main/weewx-belchertown.zip"
+FILENAME="weewx-belchertown.zip"
+
+# get upstream
+wget ${URL} -O /var/tmp/${FILENAME}
 
 echo ".....installing belchertown skin...."
 if [ -d /etc/weewx ]
 then
   # this results in /var/www/html/weewx/belchertown for the root url
-  sudo weectl extension install -y ${URL}
+  sudo weectl extension install -y /var/tmp/${FILENAME}
 elif [ -d ${HOME}/weewx-venv ]
 then
   cd ${HOME}/weewx-venv
-  source bin/activate &&  weectl extension install -y ${URL}
+  source bin/activate &&  weectl extension install -y /var/tmp/${FILENAME}
 else
   echo "skipping belchertown - can't determine dpkg or pip"
 fi

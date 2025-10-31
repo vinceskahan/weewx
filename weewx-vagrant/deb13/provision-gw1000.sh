729589037a9681ddd8a11e50b6b1df2bdce981c1
Vince Skahan <vinceskahan@gmail.com>
2025-10-31 11:08:36 -0700

    update gw1000 installer per testing

--- weewx-vagrant/deb13/provision-gw1000.sh
+++ weewx-vagrant/deb13/provision-gw1000.sh
@@ -2,17 +2,22 @@
 
 # this is gw1000 driver from my stash of the upstream repo
 
-URL="https://github.com/vinceskahan/upstream-repos/blob/main/weewx-gw1000.tgz"
+URL="https://github.com/vinceskahan/upstream-repos/raw/refs/heads/main/weewx-gw1000.tgz"
+FILENAME="weewx-gw1000.tgz"
 
-echo ".....installing belchertown skin...."
+# get upstream
+wget ${URL} -O /var/tmp/${FILENAME}
+
+echo ".....installing gw1000 skin...."
 if [ -d /etc/weewx ]
 then
   # this results in /var/www/html/weewx/belchertown for the root url
-  sudo weectl extension install -y ${URL}
+  sudo apt update && sudo apt install -y python3-six
+  sudo weectl extension install -y /var/tmp/${FILENAME}
 elif [ -d ${HOME}/weewx-venv ]
 then
   cd ${HOME}/weewx-venv
-  source bin/activate &&  weectl extension install -y ${URL}
+  source bin/activate &&  pip3 install six && weectl extension install -y /var/tmp/${FILENAME}
 else
   echo "skipping gw1000 - can't determine dpkg or pip"
 fi
